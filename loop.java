43e445ee14ca5c8dc50383d2ce937d4566e79d0a
coderjahid <jahidul5035@gmail.com>
2025-02-19 15:25:19 +0600

    basicloop

---
+++ loop.java
@@ -0,0 +1,20 @@
+public class loop {
+
+    public static void main (String arg[]){
+
+        for(int count =0; count < 100;count ++){
+
+            System.out.println("java vai");
+        }
+        int i= 0;
+        while(i < 11){
+            System.out.println(i);
+            i = i + 1;
+        }
+        do{
+            System.out.println(i);
+            i = i+1;
+        }while(i <11);
+    }
+    
+}
